367773682514070879e8327416439020c8d36127
German <72597058+GM-GitH@users.noreply.github.com>
2022-12-04 16:00:42 +0100

    Add Episode page and style

--- src/pages/Episode.js
+++ src/pages/Episode.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useContext, useState } from "react";
+import { useParams } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import PodcastLeftCard from "../components/PodcastLeftCard";
+import { PodcastContext } from "../context/PodcastContext";
 
 function Episode() {
-  return (
-    <div>Episode</div>
-  )
-}
+  const [loaded, setLoaded] = useState(false);
+  const { podcast } = useContext(PodcastContext);
+  const params = useParams();
+  const podcaster = podcast.id ?? JSON.parse(localStorage.getItem("PodcastData")).id;
+  const episodesList = JSON.parse(localStorage.getItem(podcaster));
+  const result = episodesList.value.filter((item) => item.guid.replace(/[:/]/gi, "") === params.id);
 
-export default Episode
+  if (!loaded) {
+    setLoaded(true);
+  } else {
+    return (
+      <>
+        <Navbar loaded={loaded} />
+        <PodcastLeftCard />
+        <div className="episode__container">
+          <h2>{result[0].title}</h2>
+          <div dangerouslySetInnerHTML={{ __html: result[0].description }}></div>
+          <br />
+          <br />
+          <audio controls>
+            <source src={result[0].enclosure.link} type="audio/mpeg" />
+          </audio>
+        </div>
+      </>
+    );
+  }
+}
+export default Episode;
