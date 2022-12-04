7ee6e8ff6a868f14742172c0efc46a732f27c3f1
German <72597058+GM-GitH@users.noreply.github.com>
2022-12-04 15:59:48 +0100

    Add Navbar loader

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom";
 
-function Navbar() {
+function Navbar({ loaded }) {
+  const loader = () => {
+    if (!loaded) {
+      return <div className="navbar__loader"></div>;
+    }
+  };
 
   return (
     <nav className="navbar">
       <Link to="/" className="navbar__link">
         <h2 className="navbar__link__title">Podcaster</h2>
       </Link>
+      {loader()}
       <hr />
     </nav>
   );
