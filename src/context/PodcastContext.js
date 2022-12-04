4cd36fe3548ca00454d18f5702f580b6e17d0e34
German <72597058+GM-GitH@users.noreply.github.com>
2022-12-04 14:11:36 +0100

    Set Context initialState and reducers to Filter and next components

--- src/context/PodcastContext.js
+++ src/context/PodcastContext.js
@@ -4,7 +4,13 @@ import { PodcastReducer } from "./PodcastReducer";
 
 const initialState = {
   podcast: {
+    id: null,
+    image: null,
+    title: null,
+    artist: null,
+    description: null,
     filteredTitles: 100,
+    loaded: false,
   },
 };
 export const PodcastContext = createContext(initialState.podcast);
@@ -12,6 +18,24 @@ export const PodcastContext = createContext(initialState.podcast);
 export const ContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(PodcastReducer, initialState);
 
+  function setPodcast(Id, Image, Title, Artist, Description) {
+    dispatch({
+      type: "SET_PODCAST",
+      payload: {
+        id: Id,
+        image: Image,
+        title: Title,
+        artist: Artist,
+        description: Description,
+      },
+    });
+  }
+  function setLoaded(Boolean) {
+    dispatch({
+      type: "SET_LOADED",
+      payload: Boolean,
+    });
+  }
   function setFilteredTitles(Number) {
     dispatch({
       type: "SET_FILTERED_TITLES",
@@ -23,8 +47,16 @@ export const ContextProvider = ({ children }) => {
     <PodcastContext.Provider
       value={{
         podcast: {
+          id: state.podcast.id,
+          image: state.podcast.image,
+          title: state.podcast.title,
+          artist: state.podcast.artist,
+          description: state.podcast.description,
           filteredTitles: state.podcast.filteredTitles,
+          loaded: state.podcast.loaded,
         },
+        setPodcast,
+        setLoaded,
         setFilteredTitles,
       }}
     >
--- src/context/PodcastReducer.js
+++ src/context/PodcastReducer.js
@@ -1,5 +1,25 @@
 export function PodcastReducer(state, action) {
   switch (action.type) {
+    case "SET_PODCAST":
+      return {
+        ...state,
+        podcast: {
+          ...state.podcast,
+          id: action.payload.id,
+          image: action.payload.image,
+          title: action.payload.title,
+          artist: action.payload.artist,
+          description: action.payload.description,
+        },
+      };
+    case "SET_LOADED":
+      return {
+        ...state,
+        podcast: {
+          ...state.podcast,
+          loaded: action.payload,
+        },
+      };
     case "SET_FILTERED_TITLES":
       return {
         ...state,
