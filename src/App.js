fb675c220aee0f77c4cba04805966ec383fd7ea8
German <72597058+GM-GitH@users.noreply.github.com>
2022-12-04 13:46:29 +0100

    Add App and Index to SRC folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
-import Podcast from "./pages/Podcast";
+import Podcasts from "./pages/Podcasts";
 import Episode from "./pages/Episode";
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <Router basename="/apple_music">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/podcast/:id" element={<Podcast />} />
+        <Route path="/podcast/:id" element={<Podcasts />} />
         <Route path="/podcast/:id/episode/:id" element={<Episode />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
